eed3ade2df7756af3c613c0efd8da54447d8eca8
root <alex.krajn@gmail.com>
2019-10-03 21:10:30 +0300

    Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,63 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="entrance">
+        <h1>Register</h1>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="entrance__email bl-input">
+                <label for="e-mail">E-mail Address</label>
+                <input id="e-mail" type="email" name="email" value="{{ old('email') }}" required autocomplete="email"/>
+            </div>
+            <div class="entrance__pass bl-input">
+                <label for="pass">Password</label>
+                <input id="pass" type="password" name="password" required autocomplete="new-password"/>
+            </div>
+            <div class="entrance__pass bl-input">
+                <label for="pass_conf">Confirm Password</label>
+                <input id="pass_conf" type="password" />
+            </div>
+            <div class="entrance__btn">
+                <div class="entrance__btn-login">
+                    <input type="submit" value="Register" name="password_confirmation" required autocomplete="new-password">
                 </div>
             </div>
-        </div>
+        </form>
     </div>
-</div>
-@endsection
+    @endsection
