abfdc51f33f331e019827cdf571093efa4a1035b
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-08 21:11:33 +0300

    Update

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -1,15 +1,15 @@
 @if ($paginator->lastPage() > 1)
 <ul class="pagination">
     <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
-        <a href="{{ $paginator->url(1) }}?item_count={{$itemCount}}">Previous</a>
+        <a href="{{ $paginator->url(1) }}&item_count={{$itemCount}}">Previous</a>
     </li>
     @for ($i = 1; $i <= $paginator->lastPage(); $i++)
         <li class="{{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
-            <a href="{{ $paginator->url($i) }}?item_count={{$itemCount}}">{{ $i }}</a>
+            <a href="{{ $paginator->url($i) }}&item_count={{$itemCount}}">{{ $i }}</a>
         </li>
     @endfor
     <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
-        <a href="{{ $paginator->url($paginator->currentPage()+1) }}?item_count={{$itemCount}}" >Next</a>
+        <a href="{{ $paginator->url($paginator->currentPage()+1) }}&item_count={{$itemCount}}" >Next</a>
     </li>
 </ul>
 @endif
