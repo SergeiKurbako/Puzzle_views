bd46a1d72c5acbba30dc992cc831355717df14b8
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-13 16:23:34 +0300

    Update

--- public/js/number-phone-val.js
+++ public/js/number-phone-val.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
             $('.war-phone').css({'opacity':'1'});
         }else{
             $.ajax({
-                url: "http://partycamera.org/lidsystem/check-have-phone?phone="+phone.val(),
+                url: "http://194.87.145.192/lidsystem/check-have-phone?phone="+phone.val(),
                 success: function(data){
                   if(data != 'true'){
                     $('#btn-input').click();
