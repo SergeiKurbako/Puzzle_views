1de4e05e045aa410bc2b4bd70e4bfbfc9ef463ae
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-14 13:16:32 +0300

    Update

--- Modules/AdminDashboard/Resources/views/layouts/master.blade.php
+++ Modules/AdminDashboard/Resources/views/layouts/master.blade.php
@@ -128,7 +128,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datepicker/0.6.5/datepicker.min.js"></script>
     
-    <!-- <script src="../../../js/lib/jquery.sumoselect.min.js"></script> -->
+   
 
     <script src="../../../js/admin.js"></script>
     
