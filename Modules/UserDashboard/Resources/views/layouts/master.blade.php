cac4656df6cf89645b128c8db73526106f8d4a09
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-14 13:33:11 +0300

    Update

--- Modules/UserDashboard/Resources/views/layouts/master.blade.php
+++ Modules/UserDashboard/Resources/views/layouts/master.blade.php
@@ -115,7 +115,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    
+    <script src="../../../js/lib/jquery.sumoselect.min.js"></script>
     <script src="../../../js/admin.js"></script>
 </body>
 </html>
