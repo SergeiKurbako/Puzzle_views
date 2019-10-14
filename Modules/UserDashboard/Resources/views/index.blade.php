d2d5be248379087149c74d0211e10111eddf69ff
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-14 13:18:37 +0300

    Update

--- Modules/UserDashboard/Resources/views/index.blade.php
+++ Modules/UserDashboard/Resources/views/index.blade.php
@@ -11,17 +11,7 @@
                     <h1>Пользователи</h1>
                 </div>
                 <div class="main__table">
-                    <div class="main__table--select">
-                        Показать
-                        <select>
-                            <option value="10">10</option>
-                            <option value="25">25</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                        </select>
-                        записей
-                    </div>
-
+                    
                     <div class="main__table--table">
                         <table>
                             <tr>
