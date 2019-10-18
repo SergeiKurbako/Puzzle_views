5a968e52b64ea2e92d94ec3af27803a8c8b6478d
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-18 09:46:30 +0300

    Update

--- public/js/number-sms-val.js
+++ public/js/number-sms-val.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
               headers: { "Accept": "application/text"},
               type: "GET",
               dataType: 'text',
-                url: "http://partycamera.org/lidsystem/check-right-sms-code?sms_code="+sms.val(),
+                url: "http://194.87.145.192/lidsystem/check-right-sms-code?sms_code="+sms.val(),
                 // url: "https://partycamera.org/lidsystem/check-right-sms-code?sms_code="+sms.val(),
                 // url: "http://127.0.0.2/lidsystem/check-right-sms-code?sms_code="+sms.val(),
                 
