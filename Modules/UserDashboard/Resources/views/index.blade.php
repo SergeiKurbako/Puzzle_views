256dcc752b4acac76a240605598d8ab0908c44c4
Aleksandr-krajn <alex.krajn@gmail.com>
2019-10-24 21:41:21 +0300

    Updd

--- Modules/UserDashboard/Resources/views/index.blade.php
+++ Modules/UserDashboard/Resources/views/index.blade.php
@@ -44,10 +44,10 @@
 
                                 <td class="td__code">
                                     <div class="code-frame">
-                                        <xmp><script src="http://partycamera.org/buttonframe/repack.js"></script>
+                                        <xmp><script src="http://admin.webwidgets.ru/buttonframe/repack.js"></script>
                                         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" defer></script>
-                                        <script src="https://partycamera.org/buttonframe/js/mb.js" defer></script>
-                                        <script src="https://partycamera.org/buttonframe/js/main.js" defer></script>
+                                        <script src="http://admin.webwidgets.ru/buttonframe/js/mb.js" defer></script>
+                                        <script src="http://admin.webwidgets.ru/buttonframe/js/main.js" defer></script>
                                         <script>
                                             var src = `{{stripos($_SERVER["SERVER_PROTOCOL"],"https") === 0 ? "https://" : "http://" . $_SERVER['HTTP_HOST'] . "/lidsystem/?frame_id=" . $frame->id . "&code=" . $frame->code}}`;
                                             document.getElementById('iframe').src = src;
